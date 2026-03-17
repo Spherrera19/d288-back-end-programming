ca5f237b8ef19c0c8a2702ffe84345a45bcb6002
spherrera19 <Steven.Herrera.0519@Gmail.com>
2026-03-17 00:10:52 -0400

    Step B Initial commit - Added Spring Boot skeleton

---
+++ src/test/java/com/example/stevend288/StevenD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stevend288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StevenD288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
