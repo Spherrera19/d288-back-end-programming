2c6f844ee05f68470a59d1ea02c2f9e76a100484
spherrera19 <Steven.Herrera.0519@Gmail.com>
2026-03-23 13:42:41 -0400

    Verified override to ensure Lombok 1.18.36 is used

---
+++ src/main/resources/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.example.stevend288.controllers;
+
+import com.example.stevend288.services.CheckoutService;
+import com.example.stevend288.services.Purchase;
+import com.example.stevend288.services.PurchaseResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        return checkoutService.placeOrder(purchase);
+    }
+}
